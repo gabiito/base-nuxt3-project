85e2c1803e76d3032f06fea495232d25338b5d8d
Juan Vargas <juanmvg2006@gmail.com>
2023-01-03 13:30:49 -0300

    Add pages directory with root component

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-full h-screen flex justify-center items-center">
-    <h1 class="text-4xl">{{ $config.appName }}</h1>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <h1 class="text-4xl">{{ $config.appName }}</h1>
+  </div>
+</template>
