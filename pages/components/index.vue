344768450f255c98fa68cdd4612866537c5ce278
Juan Vargas <juanmvg2006@gmail.com>
2023-03-22 10:01:33 -0300

    Add components page

---
+++ pages/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageWrapper>
+    <div class="w-full flex justify-center">
+      <div class="w-1/2">
+        <InputField label="Name" />
+      </div>
+    </div>
+  </PageWrapper>
+</template>
+
+<script setup>
+import PageWrapper from '@/components/layout/wrappers/PageWrapper'
+import InputField from '@/components/common/inputs/InputField'
+</script>
