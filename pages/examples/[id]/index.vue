515258296bf7b232f6b2f86c3bc1de6b41817826
Juan Vargas <juanmvg2006@gmail.com>
2023-03-29 15:53:05 -0300

    Update example markup

--- pages/examples/[id]/index.vue
+++ pages/examples/[id]/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div v-if="!loading" class="w-full">
-    <Card v-if="current" class="p-2">
-      {{ current.title }}
-    </Card>
-  </div>
-  <div v-else>Loading...</div>
+  <PageWrapper>
+    <div v-if="!loading" class="w-full mt-10">
+      <Card v-if="current" class="p-4"> Title: {{ current.title }} </Card>
+    </div>
+    <div v-else>Loading...</div>
+  </PageWrapper>
 </template>
 
 <script setup>
+import PageWrapper from '@/components/layout/wrappers/PageWrapper'
 import Card from '@/components/common/others/Card'
+
 import { useExamplesStore } from '@/stores/examples'
 
 /* ----------------------------------------------------------------- */
--- pages/examples/index.vue
+++ pages/examples/index.vue
@@ -1,24 +1,28 @@
 <template>
-  <div v-if="!loading" class="w-full">
-    <div v-if="items.length > 0" class="space-y-4 mt-10">
-      <Card
-        v-for="item in items"
-        :key="item.id"
-        class="p-2"
-        @click="router.push({ path: `/examples/${item.id}` })"
-      >
-        {{ item.title }}
-      </Card>
+  <PageWrapper>
+    <div v-if="!loading" class="w-full">
+      <div v-if="items.length > 0" class="space-y-4 mt-10">
+        <Card
+          v-for="item in items"
+          :key="item.id"
+          class="p-4 cursor-pointer"
+          @click="router.push({ path: `/examples/${item.id}` })"
+        >
+          Title: {{ item.title }}
+        </Card>
+      </div>
+      <div v-else class="w-full mt-10 flex justify-center items-center">
+        No elements to show
+      </div>
     </div>
-    <div v-else class="w-full mt-10 flex justify-center items-center">
-      No elements to show
-    </div>
-  </div>
-  <div v-else>Loading...</div>
+    <div v-else>Loading...</div>
+  </PageWrapper>
 </template>
 
 <script setup>
+import PageWrapper from '@/components/layout/wrappers/PageWrapper'
 import Card from '@/components/common/others/Card'
+
 import { useExamplesStore } from '@/stores/examples'
 
 /* ----------------------------------------------------------------- */
