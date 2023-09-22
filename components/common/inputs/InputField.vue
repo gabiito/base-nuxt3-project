31fce12bfcb415ed0408a1d29a56e6bfcfe386ba
Gabriel Zerbino <gabozerbino@gmail.com>
2023-09-22 11:56:44 -0300

    Update: moved the template to the top

--- components/common/inputs/InputField.vue
+++ components/common/inputs/InputField.vue
@@ -1,3 +1,53 @@
+<template>
+  <div class="w-full">
+    <label
+      v-if="label !== ''"
+      :for="state.uniqueId"
+      class="block text-sm font-medium leading-6 text-gray-900"
+      >{{ label }}</label
+    >
+    <div class="relative mt-2">
+      <div
+        v-if="hasLeftIcon"
+        class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+      >
+        <slot name="leftIcon"></slot>
+      </div>
+      <input
+        v-bind="$attrs"
+        :id="state.uniqueId"
+        :value="modelValue"
+        class="w-full shadow-inner shadow-slate-300 outline bg-gray-50 rounded-md py-2 px-3 text-sm"
+        :class="{
+          'focus:outline-secondary-500 text-gray-600 placeholder-gray-300':
+            error === '',
+          'text-rose-400 placeholder:text-rose-300 outline-rose-300 focus:outline-rose-500':
+            error !== '',
+          'outline-green-300 focus:outline-green-500': success,
+          'outline-orange-300 focus:outline-orange-500': warning,
+          'outline-blue-300 focus:outline-blue-500': info,
+          'outline-transparent': state.emptyState,
+          'pl-10': hasLeftIcon,
+        }"
+        @input="updateInput"
+      />
+      <div
+        v-if="error !== ''"
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
+      >
+        <ExclamationIcon class="text-rose-500" />
+      </div>
+    </div>
+    <p
+      v-if="error !== ''"
+      id="email-error"
+      class="mt-2 text-sm text-rose-500 capitalize"
+    >
+      {{ error }}
+    </p>
+  </div>
+</template>
+
 <script>
 export default {
   inheritAttrs: false,
@@ -76,55 +126,5 @@ onMounted(() => {
     !props.success && !props.warning && !props.info && props.error === ''
 })
 </script>
-
-<template>
-  <div class="w-full">
-    <label
-      v-if="label !== ''"
-      :for="state.uniqueId"
-      class="block text-sm font-medium leading-6 text-gray-900"
-      >{{ label }}</label
-    >
-    <div class="relative mt-2">
-      <div
-        v-if="hasLeftIcon"
-        class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-      >
-        <slot name="leftIcon"></slot>
-      </div>
-      <input
-        v-bind="$attrs"
-        :id="state.uniqueId"
-        :value="modelValue"
-        class="w-full shadow-inner shadow-slate-300 outline bg-gray-50 rounded-md py-2 px-3 text-sm"
-        :class="{
-          'focus:outline-secondary-500 text-gray-600 placeholder-gray-300':
-            error === '',
-          'text-rose-400 placeholder:text-rose-300 outline-rose-300 focus:outline-rose-500':
-            error !== '',
-          'outline-green-300 focus:outline-green-500': success,
-          'outline-orange-300 focus:outline-orange-500': warning,
-          'outline-blue-300 focus:outline-blue-500': info,
-          'outline-transparent': state.emptyState,
-          'pl-10': hasLeftIcon,
-        }"
-        @input="updateInput"
-      />
-      <div
-        v-if="error !== ''"
-        class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
-      >
-        <ExclamationIcon class="text-rose-500" />
-      </div>
-    </div>
-    <p
-      v-if="error !== ''"
-      id="email-error"
-      class="mt-2 text-sm text-rose-500 capitalize"
-    >
-      {{ error }}
-    </p>
-  </div>
-</template>
 
 <style scoped></style>
